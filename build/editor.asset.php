5af13d89f44a97081b12826743139ec1d1a27d0e
Paolo Tesei <ptesei@beapi.fr>
2025-11-08 17:51:21 +0000

    Release version 1.0.4

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'dfb7cf8b31d3c797e2cc');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '402045eb23196dfd89e7');
